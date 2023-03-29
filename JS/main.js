cd6a091703e42f902724367fe6391c830d25b4fb
scuffedplus <seanspektor6@gmail.com>
2023-03-29 13:54:07 -0700

    new color

--- JS/main.js
+++ JS/main.js
@@ -14,7 +14,7 @@ for (var i = 0; i < coll.length; i++) {
 var themeint = sessionStorage.getItem("themeint")
 var root = document.querySelector(':root');
 if(themeint == null){
-    var themeint = Math.floor(Math.random()*3);
+    var themeint = Math.floor(Math.random()*4);
     sessionStorage.setItem("themeint", themeint);
 }
 
@@ -23,22 +23,25 @@ var colors = {
         "primary-color": "#ffc996",
         "backdrop-color": "#583D72",
         "secondary-color": "#583D72",
-        "primary-double": "#ffeae2",
-        "tertiary-color": "#271b35"
+        "primary-double": "#ffeae2"
     },
     1:{
         "primary-color": "#ee8de6",
         "backdrop-color": "#0f0f0f",
         "secondary-color": "#000000",
-        "primary-double": "#5bebf5",
-        "tertiary-color": "#ffa7ff93"
+        "primary-double": "#5bebf5"
     },
     2:{
         "primary-color": "#e7723c",
         "backdrop-color": "#23272a",
         "secondary-color": "#ffffff",
-        "primary-double": "#f5a55b",
-        "tertiary-color": "##e65c00"
+        "primary-double": "#f5a55b"
+    }
+    3:{
+        "primary-color": "#fff4e6",
+        "backdrop-color": "#3c2f2f",
+        "secondary-color": "#854442",
+        "primary-double": "#ffe9cc"
     }
 }
 
@@ -50,5 +53,5 @@ root.style.setProperty('--primary-color', theme["primary-color"]);
 root.style.setProperty('--backdrop-color', theme["backdrop-color"]);
 root.style.setProperty('--secondary-color', theme["secondary-color"]);
 root.style.setProperty('--primary-double', theme["primary-double"]);
-root.style.setProperty('--tertiary-color', theme["tertiary-color"]);
+
 
